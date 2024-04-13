e82b7734fbe2b2d457e8fb33029df06d819f2fd1
Alex <whites.russia@gmail.com>
2024-04-13 13:22:27 +0300

    [fix]: пусть к key.json

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "yandex" {
     # token = ""
-    service_account_key_file = file("/root/keys/key.json")
+    service_account_key_file = "/root/keys/key.json"
     cloud_id = ""
     folder_id = ""
     zone = "ru-central1-a"
