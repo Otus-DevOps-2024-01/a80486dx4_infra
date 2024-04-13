f682c2dd287dd20a5eed6774a7f5df04ee063439
Alex <whites.russia@gmail.com>
2024-04-13 12:34:32 +0300

    [add]: создание ресурса

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,23 @@ provider "yandex" {
     folder_id = ""
     zone = "ru-central1-a"
 }
+
+resource "yandex_compute_instance" "app" {
+    name = "reddit-app"
+
+    resources {
+        cores = 1
+        memory = 2
+    }
+    boot_disk {
+        initialize_params {
+            # здесь id образа из предыдущего задания
+            image_id = "fd809rgi7iom8t5av18b"
+        }
+    }
+    network_interface {
+        # id подсети default-ru-central1-a
+        subnet_id = "e9bojkkggs4ad7ecbi2o"
+        nat = true
+    }
+}
