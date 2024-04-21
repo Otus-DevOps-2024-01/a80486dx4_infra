0924e5ef873284089a34bad5529cf3195af9fbab
Alex <whites.russia@gmail.com>
2024-04-21 12:10:47 +0300

    [ref]: разбиваем на отдельные файлы

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,7 @@ output "external_ip_address_app" {
     # network_interface.0.nat_ip_address - атрибуты ресурса
     value = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
+
+output "external_ip_address_db" {
+    value = yandex_compute_instance.db.network_interface.0.nat_ip_address
+}
