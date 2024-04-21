d4602feb51c9e1b0375dd6c46bb4cd1dd4413687
Alex <whites.russia@gmail.com>
2024-04-21 13:52:33 +0300

    [fix]: возвращаем обратно vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,20 @@ module "db" {
   subnet_id = var.subnet_id
 }
 
-module "vpc" {
-  source = "./modules/vpc"
-#   public_key_path = var.public_key_path
-#   db_disk_image = var.db_disk_image
-#   subnet_id = var.subnet_id
+# module "vpc" {
+#   source = "./modules/vpc"
+# #   public_key_path = var.public_key_path
+# #   db_disk_image = var.db_disk_image
+# #   subnet_id = var.subnet_id
+# }
+
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name = "reddit-app-subnet"
+  zone = "ru-central1-a"
+  network_id = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
