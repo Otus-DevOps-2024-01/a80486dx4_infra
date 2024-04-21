97101900f2e0ec7ee7f7b7aa042f884d7b1b6eba
Alex <whites.russia@gmail.com>
2024-04-21 14:03:57 +0300

    [fix]: переменные в outputs модулей

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -1,4 +1,4 @@
 output "external_ip_address_db" {
     # value = yandex_compute_instance.db.network_interface.0.nat_ip_address
-    value = module.db.external_ip_address_db
+    value = yandex_compute_instance.db.network_interface.0.nat_ip_address
 }
